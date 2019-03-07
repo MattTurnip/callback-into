e7b685724a50a289dcab560af32fbc2ec5002291
MattTurnip <mattadavey@gmail.com>
2019-03-07 17:25:20 +0000

    refactored code to include forEach

--- whereswaldo.js
+++ whereswaldo.js
@@ -1,18 +1,15 @@
-// The second argument/parameter is expected to be a function
 function findWaldo(arr, callback) {
-    for (var i = 0; i < arr.length; i++) {
-        var waldoLocation = 0;
-        if (arr[i] === "Waldo") {
-            waldoLocation = i;
-            callback(waldoLocation);   // execute callback
-      }
+  var waldoLocation = 0;
+  arr.forEach(function(name, i){
+    if (name === "Waldo"){
+      waldoLocation = i;
+      callback(waldoLocation)
     }
-  }
+  });
+}
   
   function actionWhenFound(i) {
     console.log("waldo is at " + i);
-
-   
   }
   
   findWaldo(["Alice", "Waldo", "Bart", "Winston"], actionWhenFound);
